e04060f25a0d2bb2cfce17e5bb1fc1740683a876
rasfsfsggsg <playerkhan0001@gmai.com>
2026-03-20 15:51:34 +0530

    Deploy Flutter Web site to GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fa6e66c1ceb97a17aa05b053390fede9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "98c175083578157ffe5786ba0ab3686f",
+"flutter_bootstrap.js": "3dc1803343f351ec39e4ef26a409454d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
